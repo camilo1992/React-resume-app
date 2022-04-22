b61063a93655c3c6211b4fba2a5baa93ea239bba
cristian camilo gutierrez correa <gccc1108@hotmail.com>
2022-04-22 16:51:58 -0500

    last

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Link } from "react-scroll/modules";
 import classes from "./NavBar.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
-
 function NavBar() {
   return (
     <div id="navBar ">
